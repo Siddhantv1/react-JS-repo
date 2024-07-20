ab7b5cba12e40d5e732dc8c3a59c084d8dc04e9b
SiddhantHTML <siddhaaaaaaant@gmail.com>
2024-07-20 12:46:06 +0530

    import card component

--- 03Props+TailwindCSS/src/App.jsx
+++ 03Props+TailwindCSS/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='bg-yellow-600 text-black p-4 rounded-xl mb-4'> Siddhant Ojha </h1>
+
+      <Card/>
 
     </>
   )
