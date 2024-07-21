036eab1d418ecf639b3ced818b69cc25f0b7a8de
SiddhantHTML <siddhaaaaaaant@gmail.com>
2024-07-21 10:16:43 +0530

    first button: red

--- 04BGchanger/src/App.jsx
+++ 04BGchanger/src/App.jsx
@@ -11,9 +11,15 @@ function App() {
     <div className="w-full h-screen duration-200"
     
     style={{backgroundColor: color}}
-    
     >
-      <div className="fixed flex flex-wrap justify-center botton-12 inset-x-0 px-2"> test</div>
+      <div className="fixed flex flex-wrap justify-center botton-12 inset-x-0 px-2">
+      <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+      
+        <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor: "red"}}
+        >RED</button>
+      </div>
+      </div>
     </div>
     </>
   )
