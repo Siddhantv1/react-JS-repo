80b00df89dabcaece4308542817c098652ad146a
SiddhantHTML <siddhaaaaaaant@gmail.com>
2024-07-22 16:24:17 +0530

    styling, input and button

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react'
-import './App.css'
+
 
 function App() {
   const [length, setLength] = useState(8)
@@ -40,7 +40,8 @@ function App() {
 
   return (
     <>
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-400 bg-black'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-7 my-8 text-orange-400 bg-black'>
+    <h1 className='text-white text-center my-3'> Password Generator </h1>
     <div className=
     'className=""flex shadow rounded-lg overflow-hidden mb-4'>
 
@@ -49,9 +50,13 @@ function App() {
       className='outline-none w-full py-1 px-3'
       placeholder='password'
       readOnly
-      >
+      />
 
-      </input>
+      <button
+      className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' 
+      >Copy
+
+      </button>
 
     </div>
   </div>
