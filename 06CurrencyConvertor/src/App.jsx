a0248d7c7d65746d7bed2a36d6dd8681e5b67456
SiddhantHTML <siddhaaaaaaant@gmail.com>
2024-07-27 19:31:38 +0530

    testing text

--- 06CurrencyConvertor/src/App.jsx
+++ 06CurrencyConvertor/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-4xl bg-black text-white'> Currency app with siddhant</h1>
+     <h1 className='text-2xl bg-black text-white'> Currency app with siddhant</h1>
     </>
   )
 }
