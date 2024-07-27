437a84078c1ebfded55257d461abe725e91b90a5
SiddhantHTML <siddhaaaaaaant@gmail.com>
2024-07-27 19:57:50 +0530

    Define and return the method

---
+++ 06CurrencyConvertor/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react"
+
+
+//make a custom hook, which would call the API to use currency info
+function useCurrencyInfo(currency) {
+
+    const [data, setData] = useState({})
+
+    useEffect(()=> { //useEffect needs 2 things, a callback and a dependency array
+
+        
+        fetch(
+    `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/@{currency}.json`)
+    
+    .then((res) => res.json() )
+    .then( (res) => setData(res[currency] ) )
+    console.log(data);
+    
+
+    }, [currency]) 
+    console.log(data);
+
+    return data
+}
+
+export default useCurrencyInfo;
