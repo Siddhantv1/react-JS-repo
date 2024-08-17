16e0aceb761203fd3c26014d1d6031528997a4e5
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 22:00:07 +0530

    card related changes

--- 07DocsMini/src/components/Foreground.jsx
+++ 07DocsMini/src/components/Foreground.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Card from "./components/Card";
+import Card from "./Card";
 function Foreground() {
   return (
-    <>
+
     <div className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10'> 
     
     <Card/>
@@ -10,8 +10,7 @@ function Foreground() {
     </div>
 
 
-   
-    </>
+ 
 
   )
 }
