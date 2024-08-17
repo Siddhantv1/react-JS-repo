978eeef0932cc3af11c29911ce0d297825867431
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 20:52:33 +0530

    cards comp

---
+++ 07DocsMini/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+      {/* // make cards */}
+  
+    </div>
+  )
+}
+
+export default Card
