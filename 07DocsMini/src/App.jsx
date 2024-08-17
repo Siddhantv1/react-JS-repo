946cd91917c261e01684a57ba9bb8190b93eb1a9
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 20:40:31 +0530

    making foreground

--- 07DocsMini/src/App.jsx
+++ 07DocsMini/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
     <Background/>
+    <div className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10'> </div>
     </div>
   )
 }
