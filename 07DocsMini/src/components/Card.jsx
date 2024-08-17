120225fca79f831945c59e9b28d4bf12b4ccb376
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 21:59:44 +0530

    card footer + bg + text + icons

--- 07DocsMini/src/components/Card.jsx
+++ 07DocsMini/src/components/Card.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import { FaRegFileAlt } from "react-icons/fa";
+import { MdOutlineFileDownload } from "react-icons/md";
 function Card() {
   return (
-    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+    <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
       {/* // make cards */}
-  
+
+    <FaRegFileAlt/> 
+    <p className='font-semibold leading-right mt-5'> dummy text bolna aunty au kya ghanti mai bajau kya </p>
+    <div className='footer absolute bottom-0 bg-sky-900 w-full left-0'>
+
+        <div className='flex items-center justify-between py-3 px-8 mb-5'>
+            <h5> .4mb</h5>
+            <span className='w-7 h-7 bg-sky-500 rounded-full flex items-center justify-center'>
+                <MdOutlineFileDownload size={'1em'}/>
+            </span>
+            
+        </div>
+
+        </div>
     </div>
   )
 }
