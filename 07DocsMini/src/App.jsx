bcece7e9e01d08f1ecb99822d2edbd04625ed8e0
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 20:30:56 +0530

    import background file

--- 07DocsMini/src/App.jsx
+++ 07DocsMini/src/App.jsx
@@ -1,12 +1,9 @@
-
+import Background from "./components/Background";
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-    <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-400 text-xl'> Docs Mini </div>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter text-zinc-950'>
-      Docs.
-      </h1>
+    <Background/>
     </div>
   )
 }
