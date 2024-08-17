61c2feab21aa3945d480c1d7f89bb4bc81ae2e27
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 20:17:35 +0530

    same stuff

--- 07DocsMini/src/App.jsx
+++ 07DocsMini/src/App.jsx
@@ -3,7 +3,9 @@
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-800'>
-      <h1 className='text-9xl'>Docs</h1>
+      <h1 className='text-9xl'>
+      Docs.
+      </h1>
     </div>
   )
 }
