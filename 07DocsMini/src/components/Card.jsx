6c6e36a02f1b31707a05312bcb5eaaab6e9c66d4
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-18 00:25:23 +0530

    finishing up

--- 07DocsMini/src/components/Card.jsx
+++ 07DocsMini/src/components/Card.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
-function Card() {
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
+
+function Card({ data, reference }) {
   return (
-    <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.1}} className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
       {/* // make cards */}
 
     <FaRegFileAlt/> 
-    <p className='font-semibold leading-right mt-5'> dummy text bolna aunty au kya ghanti mai bajau kya </p>
+    <p className='font-semibold leading-right mt-5'>{data.desc}</p>
     <div className='footer absolute bottom-0 bg-sky-900 w-full left-0'>
 
         <div className='flex items-center justify-between py-3 px-8 mb-5'>
-            <h5> .4mb</h5>
-            <span className='w-7 h-7 bg-sky-500 rounded-full flex items-center justify-center'>
-                <MdOutlineFileDownload size={'1em'}/>
-            </span>
+            <h5>{data.filesize}</h5>
+            <button className='w-7 h-7 bg-sky-500 rounded-full flex items-center justify-center'>
+                {data.close ? <IoClose/> : <MdOutlineFileDownload size={'1em'}/>}
+                
+            </button>
             
         </div>
 
         </div>
-    </div>
+    </motion.div>
   )
 }
 
--- 07DocsMini/src/components/Foreground.jsx
+++ 07DocsMini/src/components/Foreground.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, {useRef, useState } from 'react'
 import Card from "./Card";
+
 function Foreground() {
+
+  const ref = useRef(null);
+
+
+  // data material
+  const data =[
+    {
+    desc: " This is the experiment, Walrider is coming, I want my money",
+    filesize: "0.9MB",
+    close: false,
+    },
+    // object 2
+    {
+      desc: "Lost in the digital ether, a whisper of code awaits, a cosmic query hangs in the balance.",
+      filesize: "42.42KB", // A nod to the ultimate answer
+      close: false, // Forever open to exploration
+    },
+    // object 3
+    {
+      desc: "A digital artifact, a whisper of creation, a universe of possibilities contained within.",
+      filesize: "3.14159KB", // A playful nod to Pi
+      close: true, // A finite creation
+    }
+  ];
+  
   return (
 
-    <div className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10'> 
-    
-    <Card/>
-    <Card/>
-    
+    <div ref = {ref} className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10 flex gap-5 flex-wrap p-5'> 
+    {data.map((item) => (
+      <Card data = {item} reference={ref} />
+    ))}
+
     </div>
 
 
