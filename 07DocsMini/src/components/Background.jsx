323af8b61a0f39e5e8116dc05448d792406ea4d6
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-17 20:30:36 +0530

    update bg in components folder

---
+++ 07DocsMini/src/components/Background.jsx
@@ -0,0 +1,14 @@
+
+
+function Background() {
+  return (
+    <>
+      <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-400 text-xl'> Docs Mini </div>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter text-zinc-950'>
+      Docs.
+      </h1>
+    </>
+  )
+}
+
+export default Background
