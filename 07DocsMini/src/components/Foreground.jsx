d8821911ca3132fb66c3bb12b1e2d1da0ad366f2
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-18 13:43:01 +0530

    add delete func + color changes

--- 07DocsMini/src/components/Foreground.jsx
+++ 07DocsMini/src/components/Foreground.jsx
@@ -1,10 +1,14 @@
 import {useRef, useState } from 'react'
 import Card from "./Card";
+import { RiDeleteBin5Fill } from "react-icons/ri";
 
 function Foreground() {
 
+  //for drag functionality
   const ref = useRef(null);
 
+  //delete card function
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
 
   // data material
   const [cardsData, setCardsData] = useState([
@@ -38,19 +42,31 @@ function Foreground() {
     setCardsData([...cardsData, newCard]);
   };
 
+  //delete card function
+  const deleteCard = (indexToDelete) => {
+    const updatedCardsData = cardsData.filter((_, index) => index !== indexToDelete);
+    setCardsData(updatedCardsData);
+  };
+
 
   return (
     <div ref={ref} className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10 flex gap-5 flex-wrap p-5'>
       {cardsData.map((item, index) => (
-        <Card key={index} data={item} reference={ref} />
-      ))}
+  <Card key={index} data={item} reference={ref} setSelectedCardIndex={setSelectedCardIndex} index={index} />
+))}
       {/* Add Button */}
       <div style={{ position: 'absolute', bottom: '20px', right: '20px' }}>
       <button onClick={addCard} className="text-3xl rounded-full w-16 h-16 bg-blue-500 text-white flex items-center justify-center">
         +
       </button>
+
+      {/* remove card button */}
+      <button onClick={() => selectedCardIndex !== null && deleteCard(selectedCardIndex)} className={` text-xl rounded-full w-16 h-16 ${selectedCardIndex === null ? 'bg-gray-600' : 'bg-red-800'} text-white flex items-center justify-center cursor-pointer`}>
+      <RiDeleteBin5Fill />
+      </button>
     </div>
-    </div>
+
+  </div>
   );
 }
 
