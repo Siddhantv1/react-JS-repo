3ae2b3ed7480d941d71bfb10b7df68c578d0ce92
Siddhantv1 <siddhaaaaaaant@gmail.com>
2024-08-18 13:18:49 +0530

    add a + button {fix position}

--- 07DocsMini/src/components/Foreground.jsx
+++ 07DocsMini/src/components/Foreground.jsx
@@ -7,7 +7,7 @@ function Foreground() {
 
 
   // data material
-  const data =[
+  const [cardsData, setCardsData] = useState([
     {
     desc: " This is the experiment, Walrider is coming, I want my money",
     filesize: "0.9MB",
@@ -25,21 +25,33 @@ function Foreground() {
       filesize: "3.14159KB", // A playful nod to Pi
       close: true, // A finite creation
     }
-  ];
+  ]);
   
+  //add card function
+  // Function to add a new card
+  const addCard = () => {
+    const newCard = {
+      desc: "New Card Description",
+      filesize: "Unknown",
+      close: false,
+    };
+    setCardsData([...cardsData, newCard]);
+  };
+
+
   return (
-
-    <div ref = {ref} className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10 flex gap-5 flex-wrap p-5'> 
-    {data.map((item) => (
-      <Card data = {item} reference={ref} />
-    ))}
-
+    <div ref={ref} className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/10 flex gap-5 flex-wrap p-5'>
+      {cardsData.map((item, index) => (
+        <Card key={index} data={item} reference={ref} />
+      ))}
+      {/* Add Button */}
+      <div style={{ position: 'absolute', bottom: '20px', right: '20px' }}>
+      <button onClick={addCard} className="text-3xl rounded-full w-16 h-16 bg-blue-500 text-white flex items-center justify-center">
+        +
+      </button>
     </div>
-
-
- 
-
-  )
+    </div>
+  );
 }
 
 export default Foreground
