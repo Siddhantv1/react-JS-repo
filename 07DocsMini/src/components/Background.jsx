e7da192703fb911b1c2897724624ad6fd4b3bb96
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-01-28 18:34:46 +0530

    improve BG UI

--- 07DocsMini/src/components/Background.jsx
+++ 07DocsMini/src/components/Background.jsx
@@ -4,9 +4,9 @@ function Background() {
   return (
     <>
     <div className=' fixed z-[2] w-full h-screen bg-zinc-800'>
-    <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-400 text-xl'> Docs Mini </div>
+    {/* <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-400 text-xl'> Docs Mini </div> */}
       <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter text-zinc-950'>
-      Docs.
+      DocsMini.
       </h1>
 
     </div>
