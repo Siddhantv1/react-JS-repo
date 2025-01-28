fd7168b321b7164b10917252ad3ba042d713a4f4
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-01-28 18:19:48 +0530

    fix NavBar UI and finish functions

--- 07DocsMini/src/components/BottomNavBar.jsx
+++ 07DocsMini/src/components/BottomNavBar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+// BottomNavBar.jsx
+import React from 'react';
+import { RiDeleteBin5Fill } from "react-icons/ri";
 
-function BottomNavBar() {
+function BottomNavBar({ onAddClick, onDeleteClick, onCancelClick, showCancel }) {
   return (
-    <div>
-      
+    <div className="fixed bottom-20 left-1/2 transform -translate-x-1/2 w-64 h-20 bg-sky-700/30 backdrop-blur rounded-3xl flex justify-around items-center">
+      {showCancel && (
+        <button onClick={onCancelClick} className="px-4 py-2 bg-slate-700 text-white rounded-full">
+          Cancel
+        </button>
+      )}
+      <button onClick={onAddClick} className="text-3xl rounded-full w-12 h-12 bg-blue-500 text-white flex items-center justify-center">
+        +
+      </button>
+      <button onClick={onDeleteClick} className={`text-xl rounded-full w-12 h-12 ${showCancel ? 'bg-red-800' : 'bg-gray-600'} text-white flex items-center justify-center cursor-pointer`}>
+        <RiDeleteBin5Fill />
+      </button>
     </div>
-  )
+  );
 }
 
-export default BottomNavBar
+export default BottomNavBar;
