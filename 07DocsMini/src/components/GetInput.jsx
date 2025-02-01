7d273df958c0d40fcf88e6edadea011a667a100e
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-02-01 18:26:27 +0530

    decrease transition time

--- 07DocsMini/src/components/GetInput.jsx
+++ 07DocsMini/src/components/GetInput.jsx
@@ -17,14 +17,14 @@ const GetInput = ({ onClose, onSubmit, initialText, initialTag }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.2 }}
       className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10"
     >
       <motion.div
         initial={{ y: '100%' }}
         animate={{ y: 0 }}
         exit={{ y: '100%' }}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.2 }}
         className="w-full max-w-md bg-sky-900/30 backdrop-blur-lg rounded-2xl shadow-lg p-6"
       >
         <FaTimesCircle color='cyan'
